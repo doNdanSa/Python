b618ab524a929bb4ae4995ba968b0cda8b4b186e
doNdanSa <78148091+doNdanSa@users.noreply.github.com>
2021-03-05 12:02:26 +0100

    Upd

---
+++ Word Embeddings/U.S.A. Presidential Vocabulary/script.py
@@ -0,0 +1,61 @@
+import os
+import gensim
+import spacy
+from president_helper import read_file, process_speeches, merge_speeches, get_president_sentences, get_presidents_sentences, most_frequent_words
+
+# get list of all speech files
+
+
+
+# read each speech file
+
+
+
+# preprocess each speech
+
+
+
+# merge speeches
+
+
+
+# view most frequently used words
+
+
+
+# create gensim model of all speeches
+
+
+# view words similar to freedom
+
+
+
+# get President Roosevelt sentences
+
+
+
+# view most frequently used words of Roosevelt
+
+
+
+# create gensim model for Roosevelt
+
+
+# view words similar to freedom for Roosevelt
+
+
+
+# get sentences of multiple presidents
+
+
+
+# view most frequently used words of presidents
+
+
+
+# create gensim model for the presidents
+
+
+# view words similar to freedom for presidents
+
+ 
